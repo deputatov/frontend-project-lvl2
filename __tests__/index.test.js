46c72206fd96acf0d221927ea583bad1042d486f
Deputatov Nikita <deputatovn@yandex.ru>
2019-10-07 19:46:38 +0300

    add check point 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,16 +4,11 @@ import genDiff from '../src';
 
 const expected = fs.readFileSync(path.resolve(__dirname, '__fixtures__/expected'), 'utf8');
 
-const data1JSON = path.resolve(__dirname, '__fixtures__/before.json');
-const data2JSON = path.resolve(__dirname, '__fixtures__/after.json');
+const getData = (name, extension) => path.resolve(__dirname, `__fixtures__/${name}.${extension}`);
 
-const data1YML = path.resolve(__dirname, '__fixtures__/before.yml');
-const data2YML = path.resolve(__dirname, '__fixtures__/after.yml');
-
-test('gendiff JSON', () => {
-  expect(genDiff(data1JSON, data2JSON)).toBe(expected);
-});
-
-test('gendiff YML', () => {
-  expect(genDiff(data1YML, data2YML)).toBe(expected);
-});
+test.each(['json', 'yml', 'ini'])(
+  'gendiff %s',
+  (extension) => {
+    expect(genDiff(getData('before', extension), getData('after', extension))).toBe(expected);
+  },
+);
